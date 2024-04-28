aa3bf1a8880e4d504e392f4b684e42f769c4e7c5
KJ-Stack <fluke_kiatkhajorn@hotmail.com>
2024-04-28 13:44:47 +0700

    Add api service

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import PostController from './controller/post.controller';
 
 const app = express();
+app.use(express.json());
+
+app.post('/posts', PostController.createBlogPost);
+app.get('/posts', PostController.getBlogPosts);
 
 app.get('/', (req, res) => {
   res.json({
